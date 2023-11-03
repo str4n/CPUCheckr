316b4f589513a8c2e61eb9d21aaaa06a35c9427b
Kamil <Str4ngerGameOne@gmail.com>
2023-11-03 14:55:14 +0100

    feat: ProcessorDto and mappings.

---
+++ CPUCheckr.Core/DTO/ProcessorDto.cs
@@ -0,0 +1,3 @@
+﻿namespace CPUCheckr.Core.DTO;
+
+public record ProcessorDto(Guid Id, string Manufacturer, string Model, int Cores, string ClockRate, string Socket, double Price);
---
+++ CPUCheckr.Core/Mappings/ProcessorMappings.cs
@@ -0,0 +1,14 @@
+﻿using CPUCheckr.Core.Domain.Entities;
+using CPUCheckr.Core.DTO;
+
+namespace CPUCheckr.Core.Mappings;
+
+internal static class ProcessorMappings
+{
+    public static ProcessorDto AsDto(this Processor processor)
+        => new(processor.Id, processor.Manufacturer, processor.Model, processor.Cores, processor.ClockRate,
+            processor.Socket, processor.Price);
+
+    public static Processor ToEntity(this ProcessorDto dto)
+        => Processor.Create(dto.Id, dto.Manufacturer, dto.Model, dto.Cores, dto.ClockRate, dto.Socket, dto.Price);
+}
