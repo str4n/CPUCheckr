99119672b0a87eddeaeecbc6dffc26e53c1e1419
Kamil <Str4ngerGameOne@gmail.com>
2023-11-06 23:15:41 +0100

    fix: update endpoint (price model fix).

---
+++ CPUCheckr.Core/DTO/PriceDto.cs
@@ -0,0 +1,7 @@
+﻿namespace CPUCheckr.Core.DTO;
+
+public record PriceDto(double Price)
+{
+    public static implicit operator double(PriceDto price) => price.Price;
+    public static implicit operator PriceDto(double price) => new(price);
+}
