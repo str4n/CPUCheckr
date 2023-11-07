337ec7d4bfdd36196cfa298ef3526ba5165e0994
Kamil <Str4ngerGameOne@gmail.com>
2023-11-07 13:15:35 +0100

    feat: ProducesDefaultContentType attribute.

--- CPUCheckr.Api/Controllers/BaseController.cs
+++ CPUCheckr.Api/Controllers/BaseController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
+using CPUCheckr.Core.Attributes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CPUCheckr.Controllers;
 
 [ApiController]
 [Route("/api/[controller]")]
+[ProducesDefaultContentType]
 public abstract class BaseController : ControllerBase
 {
     protected ActionResult<T> OkOrNotFound<T>(T model)
---
+++ CPUCheckr.Core/Attributes/ProducesDefaultContentTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPUCheckr.Core.Attributes;
+
+public class ProducesDefaultContentTypeAttribute : ProducesAttribute
+{
+    public ProducesDefaultContentTypeAttribute(Type type) : base(type)
+    {
+    }
+
+    public ProducesDefaultContentTypeAttribute(params string[] additionalContentTypes) 
+        : base("application/json", additionalContentTypes)
+    {
+    }
+}
