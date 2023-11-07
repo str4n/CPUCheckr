c15e0aadcc4b870ec0bf428d2ead53a386413069
Kamil <Str4ngerGameOne@gmail.com>
2023-11-07 12:57:03 +0100

    feat: dtos sealed.

--- CPUCheckr.Core/DTO/PriceDto.cs
+++ CPUCheckr.Core/DTO/PriceDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace CPUCheckr.Core.DTO;
 
-public record PriceDto(double Price)
+public sealed record PriceDto(double Price)
 {
     public static implicit operator double(PriceDto price) => price.Price;
     public static implicit operator PriceDto(double price) => new(price);
